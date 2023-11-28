6188462a9f3fe82ba3fa73aac5c2d52a1d70543c
dev-Pipo <lompardiab2@gmail.com>
2023-11-28 13:43:23 -0300

    eliminate folder

--- backend/bcib_test/prestamos/serializers.py
+++
@@ -1,8 +0,0 @@
-
-from rest_framework import serializers
-from .models import SolicitudPrestamo
-
-class SolicitudPrestamoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SolicitudPrestamo
-        fields = '__all__'
